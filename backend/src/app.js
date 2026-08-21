2b7b3ca5d67b11c3e80a0ba3f9244b0605538901
Himanshu Pandey <himanshupandey2003@gmail.com>
2026-08-21 19:11:19 +0530

    changes in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,8 @@ const app=express();
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }))
 app.use(express.json({limit:"16kb"})) 
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
