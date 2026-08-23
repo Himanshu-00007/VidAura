261752ac03fad99f7ac8b49dc37c4cf97161adc8
Himanshu Pandey <himanshupandey2003@gmail.com>
2026-08-23 19:30:46 +0530

    changes in video controller

--- backend/src/controllers/video.controller.js
+++ backend/src/controllers/video.controller.js
@@ -218,7 +218,7 @@ const view = async (req, res) => {
 };
 const getUserVideos=async(req,res)=>{
   try{
-    const userId=req.user.id;
+    const userId=req.user._id;
     if(!userId){
       return res.status(400).json({ message: "usedId is required" });
     }
